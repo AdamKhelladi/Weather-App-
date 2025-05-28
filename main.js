4c230461fa8b7c03b3364ad82b52ce0ed2292a70
Adam <khelladiadem5@gmail.com>
2025-05-28 17:05:21 +0100

    Finish the project

--- main.js
+++ main.js
@@ -14,39 +14,53 @@ let weatherIcon = document.querySelector(".weather-icon");
 
 window.onload = () => {
   searchInput.focus();
-}
+};
 
 let cityName;
+let apiUrl;
 
 searchBtn.addEventListener("click", () => {
   cityName = searchInput.value;
 
   const apiKey = "8c490936c0b90fb8e7ba4aebfbd1d5c9";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-  async function checkWeather() {
-    const response = await fetch(apiUrl);
-    let data = await response.json();
-
-    // console.log(data);
-
-    city.innerHTML = data.name;
-    temp.innerHTML = data.main.temp;
-    humidity.innerHTML = data.main.humidity + "%";
-    wind.innerHTML = data.wind.speed + "<span>km/h</span>";
-
-    if (data.weather[0].main == "Clouds") {
-      weatherIcon.src = "images/clouds.png";
-    } else if (data.weather[0].main == "Clear") {
-      weatherIcon.src = "images/clear.png";
-    } else if (data.weather[0].main == "Rain") {
-      weatherIcon.src = "images/rain.png";
-    } else if (data.weather[0].main == "Drizzle") {
-      weatherIcon.src = "images/drizzle.png";
-    } else if (data.weather[0].main == "Mist") {
-      weatherIcon.src = "images/mist.png";
-    }
-  }
+  apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
   checkWeather();
 });
+
+async function checkWeather() {
+  const response = await fetch(apiUrl);
+
+  // console.log(response);
+
+  if (response.status === 404) {
+    document.querySelector(".msg").style.display = "block";
+    document.querySelector(".msg").textContent = "Invalid city name 😕";
+    document.querySelector(".weather").style.display = "none";
+    return;
+  }
+
+  let data = await response.json();
+
+  // console.log(data);
+
+  city.innerHTML = data.name;
+  temp.innerHTML = data.main.temp;
+  humidity.innerHTML = data.main.humidity + "%";
+  wind.innerHTML = data.wind.speed + "<span>km/h</span>";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "images/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "images/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "images/mist.png";
+  }
+
+  document.querySelector(".msg").style.display = "none";
+  document.querySelector(".weather").style.display = "block";
+}
