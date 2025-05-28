9155e7e05d08ea002082b2b462bf753cfe9af55e
Adam <khelladiadem5@gmail.com>
2025-05-28 16:11:13 +0100

    The card design has been completed

--- main.js
+++ main.js
@@ -4,3 +4,6 @@
   -----------------
 */
 
+const apiKey = "8c490936c0b90fb8e7ba4aebfbd1d5c9";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=germany&appid=8c490936c0b90fb8e7ba4aebfbd1d5c9&units=metric";
